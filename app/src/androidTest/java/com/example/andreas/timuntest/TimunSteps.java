1a5863b1c587b3808136e086744321d25d532646
Andreas Lukito <andreas@netzme.id>
2015-01-05 11:02:34 +0700

    Improve testing
    - Menghapus entry AndroidManifest.xml yang tidak perlu.
    - Menambahkan test mengecek salah satu view.

--- app/src/androidTest/java/com/example/andreas/timuntest/TimunSteps.java
+++ app/src/androidTest/java/com/example/andreas/timuntest/TimunSteps.java
@@ -1,6 +1,7 @@
 package com.example.andreas.timuntest;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
@@ -10,26 +11,28 @@ import cucumber.api.java.en.When;
 /**
  * Created by andreas on 1/2/15.
  */
-public class TimunSteps extends ActivityInstrumentationTestCase2<MainActivity> {
+    public class TimunSteps extends ActivityInstrumentationTestCase2<MainActivity> {
 
-    public TimunSteps() {
-        super(MainActivity.class);
-    }
+        public TimunSteps() {
+            super(MainActivity.class);
+        }
 
-    @Given("^I have a test$")
-    public void i_have_a_test() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
+        @Given("^I have a test$")
+        public void i_have_a_test() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
 
-    }
+        }
 
-    @When("^I test$")
-    public void i_test() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-    }
+        @When("^I test$")
+        public void i_test() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+        }
 
-    @Then("^I succeed$")
-    public void i_succeed() throws Throwable {
-// Write code here that turns the phrase above into concrete actions
-        assertNotNull(getActivity());
-    }
+        @Then("^I succeed$")
+        public void i_succeed() throws Throwable {
+    // Write code here that turns the phrase above into concrete actions
+            assertNotNull(getActivity());
+            assertEquals("Hello world!",
+                    ((TextView) getActivity().findViewById(R.id.text_hello)).getText());
+        }
 }
